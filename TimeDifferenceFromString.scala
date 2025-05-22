99463106af2e4c9f3757d652cab30b35bd2c412d
yurizhuravel <yuri@zhuravel.co.uk>
2025-05-22 14:10:39 +0100

    Time difference from string input

--- TimeDifferenceFromString.scala
+++ TimeDifferenceFromString.scala
@@ -1,24 +1,24 @@
 // Parse hour and minute values supplied as strings and calculate the time difference between them
 
 object TimeDifferenceFromString:
-    def getDuration(start: String, end: String): Int = {
-        import java.time.*
-        import java.time.format.DateTimeFormatter
-        import scala.math.*
-        
-        // Define the time format
-        val formatter = DateTimeFormatter.ofPattern("HH:mm")
+  def getDuration(start: String, end: String): Unit =
+    import java.time.*
+    import java.time.format.DateTimeFormatter
+    import scala.math.*
 
-        val startTime = LocalTime.parse(start, formatter)
-        val endTime = LocalTime.parse(end, formatter)
+    // Define the time format (or dd-MM-yyyy, etc. if needed)
+    val formatter = DateTimeFormatter.ofPattern("HH:mm")
 
-        val stayPeriod = Duration.between(startTime, endTime)
+    val startTime = LocalTime.parse(start, formatter)
+    val endTime   = LocalTime.parse(end, formatter)
 
-        val fullHours = stayPeriod.toHours.toInt
-        val totalHours = ceil(stayPeriod.toMinutes / 60.0).toInt
+    val stayPeriod = Duration.between(startTime, endTime)
 
-        // Calculate the total cost example where the first unfinished hour costs 3,
-        // and each additional unfinished hour costs 4
-        3 + ((fullHours - 1 + (totalHours - fullHours)) * 4)
-  }
+    val fullHours  = stayPeriod.toHours.toInt
+    val totalHours = ceil(stayPeriod.toMinutes / 60.0).toInt
+
+    // Business logic goes here, don't forget to change the return type to what you need
+    println(s"Full hours: $fullHours, including unfinished hours: $totalHours")
+    
+  end getDuration
 end TimeDifferenceFromString
